b5382fed4f4f6aff3ae13e9fdc862d15f5bb7dc1
huylv-teko <huy.lv@teko.vn>
2020-05-26 11:35:16 +0700

    Initial Commit

---
+++ AuthenticationKit/AuthenticationKit.h
@@ -0,0 +1,19 @@
+//
+//  AuthenticationKit.h
+//  AuthenticationKit
+//
+//  Created by Le Vu Huy on 5/26/20.
+//  Copyright © 2020 Teko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthenticationKit.
+FOUNDATION_EXPORT double AuthenticationKitVersionNumber;
+
+//! Project version string for AuthenticationKit.
+FOUNDATION_EXPORT const unsigned char AuthenticationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthenticationKit/PublicHeader.h>
+
+
